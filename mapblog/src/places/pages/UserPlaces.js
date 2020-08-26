561110d6049e4e0f692dff12d6edbb7bbb10e510
Osiris <potterosiris@gmail.com>
2020-08-25 22:50:05 -0400

    places components

---
+++ mapblog/src/places/pages/UserPlaces.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "my house",
+    description: "its my place",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/1b/The_judgement_of_the_dead_in_the_presence_of_Osiris.jpg",
+    address: "163 North Shore Road, Hampton, NH 03842",
+    location: {
+      lat: 42.9465818,
+      lng: -70.795169,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "my house",
+    description: "its my place",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/1b/The_judgement_of_the_dead_in_the_presence_of_Osiris.jpg",
+    address: "163 North Shore Road, Hampton, NH 03842",
+    location: {
+      lat: 42.9465818,
+      lng: -70.795169,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  useParams();
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;

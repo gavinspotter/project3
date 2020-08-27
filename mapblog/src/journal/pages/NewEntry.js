84b4cea6f680eb389f33189cfe5ead14449205a1
Osiris <potterosiris@gmail.com>
2020-08-26 23:12:18 -0400

    useCallback hook, title input and description input handlers. input placed at new journal entry

--- mapblog/src/journal/pages/NewEntry.js
+++ mapblog/src/journal/pages/NewEntry.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useCallback } from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE, VALIDATOR_MIN } from "../../shared/util/validators";
 
 const NewEntry = () => {
-  return <h1> new entry works</h1>;
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="description"
+        validators={[VALIDATOR_MIN(5)]}
+        errorText="please enter a valid description (5 characters)"
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
 };
 
 export default NewEntry;
--- mapblog/src/places/pages/NewPlace.js
+++ mapblog/src/places/pages/NewPlace.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { VALIDATOR_REQUIRE, VALIDATOR_MIN } from "../../shared/util/validators";
 import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
-        validators={[VALIDATOR_REQUIRE]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="description"
+        validators={[VALIDATOR_MIN(5)]}
+        errorText="please enter a valid description (5 characters)"
+        onInput={descriptionInputHandler}
       />
     </form>
   );

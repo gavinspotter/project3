e5f2adcbfb59f53a5d19ce5015a8f2db16e280a1
Osiris <potterosiris@gmail.com>
2020-10-20 22:26:45 -0400

    bedtime

---
+++ server/controllers/places-controllers.js
@@ -0,0 +1,68 @@
+const { v4: uuidv4 } = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "realm of the dead",
+    description: "where osiris chills",
+    location: {
+      lat: 42.9463593,
+      lng: -70.795102,
+    },
+    address: "163 northshore road hampton NH",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place: place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("could not find a place for the provided id.", 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
+exports.getPlaceById = getPlaceById;
+
+exports.getPlaceByUserId = getPlaceByUserId;
+
+exports.createPlace = createPlace;
